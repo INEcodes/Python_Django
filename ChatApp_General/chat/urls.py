f0e40bb6d0e42c0ccd3e6868b2d0f9c08dd969f5
Abhay Singh Chauhan <92663561+INEcodes@users.noreply.github.com>
2024-09-07 15:30:22 +0530

    URLs

--- ChatApp_General/chat/urls.py
+++ ChatApp_General/chat/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from chat import views as chat_views
-from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path("", chat_views.chatPage, name="chatpage.html"),
+    path("", chat_views.chatPage, name="chat-page"),
+
     # login-section
     path("auth/login/", LoginView.as_view
          (template_name="chat/LoginPage.html"), name="login-user"),
